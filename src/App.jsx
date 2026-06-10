25b79150a387f90ed82318f0b22d1a0dde2998de
Minh Anh <d.anhminh.w26@gmail.com>
2026-06-10 11:01:17 +0700

    done app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header";
+import ProductForm from "./components/ProductForm";
+import ProductTable from "./components/ProductTable";
+import products from "./data/data";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+
+      <div className="content">
+        <ProductForm />
+        <ProductTable products={products} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
